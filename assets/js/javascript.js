508442d25164a107d2592e8b4796992e7e4a3116
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-11-17 13:24:39 -0600

    add commits for qusedocode

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+// a start quiz button that also starts a timer that ticks down from 75 ses
+// when the correct answer is selected you move to the next question but if the wrong anwser is selected you move on but 3 secs is taking off your time 
+// a local storage.set item and grt item
+// document.selecter
+//need eventlistener for buttons 
+// 
