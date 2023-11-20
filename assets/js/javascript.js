ba0e286986a7264038be7c8e030097e897d51abb
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-11-20 13:45:27 -0600

    gsvr my questions id and class tags

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,23 @@
+var start = document.querySelector("#startQuiz");
+var questionText = document.getElementById('#newQuestion');
+var nextQuestion = document.querySelectorAll('section')
+var answer = document.getElementById('answers')
+
+start.addEventListener("click", function(){
+
+
+    
+})
+
+
+
+
+
+
+
+
 // a start quiz button that also starts a timer that ticks down from 75 ses
+
 // when the correct answer is selected you move to the next question but if the wrong anwser is selected you move on but 3 secs is taking off your time 
 // a local storage.set item and grt item
 // document.selecter
