af4897f343e84d1fac63ac949ec1b96bbe985a10
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-11-21 13:04:03 -0600

    added the event listener

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,15 +1,74 @@
 var start = document.querySelector("#startQuiz");
-var questionText = document.getElementById('Q1', 'Q2', 'Q3', 'Q4', 'Q5');
+// var questionText = document.getElementById('Q1', 'Q2', 'Q3', 'Q4', 'Q5');
 var nextQuestion = document.querySelectorAll('section')
-var answer = document.getElementById('answersBtm')
+// var answer = document.getElementById('answersBtm')
+var secondsLeft = 75;
 
 start.addEventListener("click", function(){
+    var timer = setInterval(function(){
+    secondsLeft--;
 
-
-
+        if (secondsLeft <= 0){
+      clearInterval(timer)
+        }
+    },1000);
 })
 
+var questions = [
 
+{
+    question:"Which one of these is part of the three functions use in Javascript, for converting data types? ",
+    answer: [
+        {Choice1: "Number", correct: false},
+        {Choice2: "boolean", correct: false},
+        {Choice3: "string", correct: false},
+        {Choice4: "All of the above", correct: true},
+    ]
+},
+
+{
+    question:"How do you create a function in JavaScript?",
+    answer: [
+        {Choice1: "Function myFunction()", correct: true},
+        {Choice2: "Function = myFunction", correct: false},
+        {Choice3: "Function: myFunction", correct: false},
+        {Choice4: "Function-myFunction", correct: false},
+    ]
+},
+
+{
+    question:"Which one will call the local.setItem element?",
+    answer: [
+        {Choice1: "local.grabItem", correct: false},
+        {Choice2: "local.callItem", correct: false},
+        {Choice3: "local.getItem", correct: true},
+        {Choice4: "function local.setItem", correct: false},
+    ]
+},
+{
+    question:"Which one will allow you to write comments in Javascript?",
+    answer: [
+        {Choice1: "comment", correct: false},
+        {Choice2: "<!-comment->", correct: false},
+        {Choice3: "//comment", correct: true},
+        {Choice4: "/?comment?/", correct: false},
+    ]
+},
+
+{
+    question:" How do you write a (Code Quiz) alert box?",
+    answer: [
+        {Choice1: "alert(code quiz)", correct: true},
+        {Choice2: "alert=code quiz", correct: false},
+        {Choice3: "alert.code quiz", correct: false},
+        {Choice4: "alert-code quiz", correct: false},
+    ]
+},
+
+
+]
+
+var questionEL = document.getElementById("")
 
 
 
